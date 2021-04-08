811bf06d2074bc245b95494908b4bd3c64b6e51a
Danielle Souza <78480991+Danielleelara@users.noreply.github.com>
2021-04-08 18:20:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function EventoA(){
+    window.open("https://github.com/Danielleelara");
+   }
+function EventoB(){
+   window.open("https://www.linkedin.com/in/danielle-souza-bb95b05b");
+ }
+function EventoC(){
+  window.open("https://api.whatsapp.com/send?phone=5585992920203");
+ }
+
+function EventoD(){
+  window.open("https://www.instagram.com/danielleelara");
+ }
